5fa52fa46cfc022a9be57daacd9729a72d776f80
Michael Doyle <michael.doyle@oforce.com>
2021-03-07 14:05:56 -0700

    will revisit graphiql user auth context later

--- lib/retro_game_log_web/resolvers/event_log.ex
+++ lib/retro_game_log_web/resolvers/event_log.ex
@@ -2,19 +2,12 @@ defmodule RetroGameLogWeb.Resolvers.EventLog do
   alias Absinthe.Relay.Connection
 
   alias RetroGameLog.EventLog
-  alias RetroGameLogWeb.Resolvers.Helpers
 
-  def list_events(%{filters: filters} = args, resolution) do
-    case Helpers.current_user(resolution) do
-      {:error, _} = error ->
-        error
-
-      current_user ->
-        EventLog.list_event_log(filters)
-        |> Connection.from_list(args)
-    end
+  def list_events(%{filters: filters} = args, _resolution) do
+    EventLog.list_event_log(filters)
+    |> Connection.from_list(args)
   end
 
-  def get_event(%{id: event_id}, %{context: %{current_user: current_user}}),
+  def get_event(%{id: event_id}, _resolution),
     do: {:ok, EventLog.get_event!(event_id)}
 end
